46a3afddfea8545e99c4a10c53888d6d2b6c2413
Ananda Arti Widigdo <79387612+ananda17gb@users.noreply.github.com>
2024-07-10 13:00:13 +0700

    Delete polybar directory

--- polybar/launch_polybar.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-if type "xrandr"; then
-  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
-    MONITOR=$m polybar --reload toph &
-  done
-else
-  polybar --reload toph &
-fi
