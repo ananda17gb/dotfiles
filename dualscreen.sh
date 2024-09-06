6fbf2cf5e16749ec0f82b3255ab1d64e0b4b34b3
Ananda Arti Widigdo <79387612+ananda17gb@users.noreply.github.com>
2024-09-06 21:03:44 +0700

    Add files via upload

---
+++ dualscreen.sh
@@ -0,0 +1,4 @@
+xrandr --output eDP-1 --mode 1920x1080 --rate 60 --auto --primary
+
+xrandr --output eDP-1-1 --mode 1920x1080 --rate 60 --auto --primary
+xrandr --output HDMI-0 --mode 1920x1080 --rate 59.94 --right-of eDP-1-1
